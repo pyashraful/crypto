e7b52cea34f85ff59c962a38699f41dd7e6388b7
Py-Ashraful <ashrafulislam180@gmail.com>
2021-09-21 22:27:05 +0600

    Layout and Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1> Hello World</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer"> </div>
     </div>
   );
 };
---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+import icon from "../image/cryptocurrency.png";
+
+const Navbar = () => {
+  return (
+    <div className="nav-c0ntainer">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+      </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default Navbar;
