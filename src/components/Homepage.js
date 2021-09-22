90ab83e0a3fe1c9fbf22d2372612fb5d70cc7012
Py-Ashraful <ashrafulislam180@gmail.com>
2021-09-22 15:24:40 +0600

    add data to view

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
@@ -6,9 +7,11 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data } = useGetCryptosQuery();
-  console.log(data);
+  const { data, isFetching } = useGetCryptosQuery();
 
+  if (isFetching) return "Loading...";
+  const globalState = data?.data?.stats;
+  console.log(globalState);
   return (
     <>
       <Title level={2} className="heading">
@@ -16,22 +19,31 @@ const Homepage = () => {
       </Title>
       <Row gutter={[32, 32]}>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={5} />
+          <Statistic title="Total Cryptocurrencies" value={globalState.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={5} />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalState.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap:" value={5} />
+          <Statistic
+            title="Total Market Cap:"
+            value={millify(globalState.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value={5} />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalState.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={5} />
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total Markets" value={5} />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalState.totalMarkets)}
+          />
         </Col>
       </Row>
     </>
