f79abcc736510f600d2c172b1f7ffd3179d8cd37
Py-Ashraful <ashrafulislam180@gmail.com>
2021-09-23 12:42:45 +0600

    news component

--- src/components/News.js
+++ src/components/News.js
@@ -1,23 +1,48 @@
+import React, { useState } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
-import React from "react";
-import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 import moment from "moment";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 import Loader from "./Loader";
 
-const { Text, Title } = Typography;
-const { Option } = Select;
 const demoImage =
   "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
 
+const { Text, Title } = Typography;
+const { Option } = Select;
+
 const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data } = useGetCryptosQuery(100);
   const { data: cryptoNews } = useGetCryptoNewsQuery({
-    newsCategory: "Cryptocurrency",
+    newsCategory,
     count: simplified ? 6 : 12,
   });
+
   if (!cryptoNews?.value) return <Loader />;
-  console.log(cryptoNews);
+
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurency">Cryptocurrency</Option>
+            {data?.data?.coins?.map((currency) => (
+              <Option value={currency.name}>{currency.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
